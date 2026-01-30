cab5e85e642c117983f4b6d9d974a9f5a232bd82
devopsuii <ahsandevops8@gmail.com>
2026-01-30 15:17:38 +0500

    Revert "Staging-into-main "

--- __init__.py
+++ __init__.py
@@ -1,13 +1,11 @@
-from . import 
+from . import models
 from . import controllers
 from odoo.addons.payment import reset_payment_provider
 
 
 def uninstall_hook(env):
     """
-    Uninstall hook to clean up Revolut payment provider configuration.......................................
-
-Now the action will work
+    Uninstall hook to clean up Revolut payment provider configuration.
 
     This function is called automatically when the module is uninstalled.
     It removes any links or settings related to the 'revolut' provider
@@ -16,7 +14,3 @@ Now the action will work
     :param env: Odoo environment passed during module uninstall
     """
     reset_payment_provider(env, "revolut")
-this is a testing code .............................
------------------
-github actions
-again testging github actions
